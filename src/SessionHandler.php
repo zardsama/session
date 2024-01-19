744e79d0b058533e865d3f9d633960a734f42072
zardsama <zardsama@naver.com>
2024-01-20 04:51:25 +0900

    QueryBuilder

--- src/SessionHandler.php
+++ src/SessionHandler.php
@@ -1,20 +1,23 @@
 <?php
 
-namespace zardsama\session;
-
 /**
  * DB 세션 핸들러 클래스
  **/
 
+namespace zardsama\session;
+
 abstract class SessionHandler
 {
 
-    private $cookie_domain;
+    private string $session_name;
+    private string $cookie_domain;
 
     /**
      * 세션 핸들러 등록
+     * @param string|null $session_name
+     * @return void
      */
-	protected function init()
+	protected function init(string | null $session_name) : void
     {
         if (ini_get('session.auto_start') == 1) {
             session_write_close();
@@ -41,26 +44,31 @@ abstract class SessionHandler
         ]);
 
         session_start();
+        if ($session_name) {
+            session_name($session_name);
+        }
+        $this->session_name = session_name();
 	}
 
     /**
      * 세션 내용 분석
+     * @param string $session_data
+     * @return array
      */
-    public function unserialize($session_data)
+    public function unserialize(string $session_data) : array
     {
-         $return_data = array();
+         $return_data = [];
          $offset = 0;
          while ($offset < strlen($session_data)) {
-             if (!strstr(substr($session_data, $offset), "|"))
-             {
-                 return;
+             if (!strstr(substr($session_data, $offset), "|")) {
+                 return [];
              }
              $pos = strpos($session_data, "|", $offset);
              $num = $pos - $offset;
-             $varname = substr($session_data, $offset, $num);
+             $var_name = substr($session_data, $offset, $num);
              $offset += $num + 1;
              $data = unserialize(substr($session_data, $offset));
-             $return_data[$varname] = $data;
+             $return_data[$var_name] = $data;
              $offset += strlen(serialize($data));
          }
          return $return_data;
@@ -68,48 +76,61 @@ abstract class SessionHandler
 
     /**
      * 세션 열기
+     * @param string $savePath
+     * @param string $sessionName
+     * @return bool
      */
-    abstract public function open($savePath, $sessionName);
+    abstract public function open(string $path, string $name): bool;
 
     /**
      * 세션 닫기
+     * @return bool
      */
-    abstract public function close();
+    abstract public function close(): bool;
 
     /**
      * 세션 읽기
      * PHP7부터 값이 null 로 리턴되면 세션이 동작하지 않습니다.
      * 값이 없더라도 빈 스트링으로 전송해야 합니다.
+     * @param string $id
+     * @return string
      */
-    abstract public function read($id);
-
+    abstract public function read(string $id) : string|false;
 
     /**
      * 세션 쓰기
+     * @param string $id
+     * @param string $data
+     * @return bool
      */
-    abstract public function write($id, $data);
+    abstract public function write(string $id, string $data) : bool;
 
     /**
      * 세션 삭제
+     * @param string $id
+     * @return bool
      */
-    abstract public function destroy($id);
+    abstract public function destroy(string $id) : bool;
 
     /**
      * 만료 된 세션 삭제
+     * @param int $maxlifetime
+     * @return int|false
      */
-    abstract public function gc($maxlifetime);
+    abstract public function gc(int $max_lifetime) : int|false;
 
     /**
      * 지정된 세션 아이디가 존재하는지 리턴
+     * @param string $id
+     * @return bool
      */
-    abstract public function exists($id);
+    abstract public function exists(string $id) : bool;
 
     /**
      * 세션의 내용을 배열로 리턴
+     * @param string $id
+     * @return array
      */
-    abstract public function parse($id);
+    abstract public function parse(string $id) : array;
 
 }
-
-
-?>
